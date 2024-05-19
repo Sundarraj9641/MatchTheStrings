b15cbcb86dfe5f89b931f8f1af93d8e55f082dfe
Mahalingam <sundarraj.mahalingam@capgemini.com>
2024-05-19 10:13:14 +0530

    pushed by sundarraj

---
+++ src/com/match/Concat.java
@@ -0,0 +1,15 @@
+package com.match;
+
+public class Concat {
+
+	public static void main(String[] args) {
+		
+		String s1 = "plabani";
+		String s2 = "Gernany";
+		
+		String s3=s1.concat(s2);
+		System.out.println(s3);
+
+	}
+
+}
